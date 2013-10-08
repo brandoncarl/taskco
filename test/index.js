09b9c5d7a8007047c295d5fa0b9f79df6bee01ea
Brandon Carl <brandon.j.carl@gmail.com>
2013-10-08 16:54:38 -0400

    irresponsibly large number of changes

--- test/index.js
+++ test/index.js
@@ -2,24 +2,57 @@
 var TaskCo = require('../index.js').setup();
 // var worker = require('./worker');
 
-var processEmail = function(data, done) {
-  console.log('processing');
-  done()
+var processEmail = function(task, done) {
+  console.log('PROCESSING JOB', task.id);
+  setTimeout(done, 500);
 }
 
 
 TaskCo.addProcedure('email', processEmail);
 
-TaskCo.addTeam('email', function() { this.start(); });
+TaskCo.addTeam('email', 1, function(err, team) { team.start(); });
+
+// TaskCo.quickTask
+
+var id;
+
+// setTimeout(function() {
+
+//   TaskCo.createTask('email', {}).then(function(task) {
+
+//     // Store task id
+//     id = task.id;
+
+//     // Attach listeners
+//     task.on('complete', function() {
+//       console.log("completed");
+//       // Free memory
+//       task = null;
+//     });
+
+//     // Save
+//     task.save();
+//   });
+
+// }, 500)
+
 
 setTimeout(function() {
-  TaskCo.createTask('email', {}, function(task) {
-    console.log(task);
-  });
+
+  for (var i = 0; i < 10; ++i) {
+    TaskCo.shortcut('email', {}).then(function(id) {
+      console.log('TASK CREATED WITH ID', id);
+    });
+  }
+
 }, 500)
 
+
+
 setTimeout(function() {
-  TaskCo.createTask('email', {}, function(task) {
-    console.log(task);
+
+  TaskCo.shortcut('email', {}).then(function(id) {
+
   });
-}, 1500)
+
+}, 5000);
